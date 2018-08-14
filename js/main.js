ce7e93c5ffb71f1b7de09f8061d61a1c100b183a
Randy Laub <laubrk@gmail.com>
2018-08-13 22:52:42 -0700

    css responsive changes to home page

--- js/main.js
+++ js/main.js
@@ -78,7 +78,7 @@ initMap = () => {
         scrollWheelZoom: false
       });
   L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-    mapboxToken: '<your MAPBOX API KEY HERE>',
+    mapboxToken: 'pk.eyJ1IjoibGF1YnJrIiwiYSI6ImNqa3J0Nm40ZTEzdXgzdmxteW4xMTZmb2sifQ.5FzDrGK6q9aYlpzCMXMurQ',
     maxZoom: 18,
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
       '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
